7a4d5ed57e5b1e65eeaea904c391c6caa5d5dae5
Richard Olsson <r@richardolsson.se>
2017-02-13 16:21:40 +0100

    Add yml2tsv script for localization

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Zetkin developer utility scripts
 This repository contains scripts that aid in development of the Zetkin apps and
 platform.
+
+* l10n – scripts to be used in the localization process
---
+++ l10n/yml2tsv/index.js
@@ -0,0 +1,94 @@
+'use strict';
+
+const flatten = require('flat').flatten;
+const yaml = require('node-yaml');
+const recurse = require('recursive-readdir');
+const stringify = require('csv-stringify');
+
+
+let loadMessages = (path, cb) => {
+    let messages = {};
+    let numCompleted = 0;
+    let ignore = (file, stats) =>
+        !stats.isDirectory() && !file.endsWith('.yaml');
+
+    recurse(path, [ ignore ], (err, files) => {
+        if (err) {
+            console.log('readdir() error', err);
+            return;
+        }
+
+        for (let i = 0; i < files.length; i++) {
+            yaml.read(files[i], null, (err, data) => {
+                if (err) {
+                    cb(err, null);
+                }
+                else {
+                    let relPath = files[i].replace(path, '')
+
+                    let pathElems = relPath
+                        .substring(0, relPath.length - 5)
+                        .split('/');
+
+                    let filePath = pathElems
+                        .slice(0, pathElems.length -1)
+                        .join('.');
+
+                    let fileMessages = flatten(data);
+                    let flatData = flatten(data);
+                    Object.keys(flatData).forEach(msg => {
+                        let lang = pathElems[pathElems.length-1];
+                        let fullPath = filePath + '.' + msg;
+
+                        if (!messages.hasOwnProperty(fullPath)) {
+                            messages[fullPath] = {
+                                filePath: filePath,
+                                yamlPath: msg,
+                                translations: {},
+                            };
+                        }
+
+                        messages[fullPath].translations[lang] = flatData[msg];
+                    });
+                }
+
+                numCompleted++;
+                if (numCompleted === files.length) {
+                    cb(null, messages);
+                }
+            });
+        }
+    });
+};
+
+loadMessages(process.argv[2], (err, messages) => {
+    if (err) {
+        console.log('Load error', err);
+    }
+
+    let langs = [];
+    Object.keys(messages).forEach(msg => {
+        Object.keys(messages[msg].translations).forEach(lang => {
+            if (langs.indexOf(lang) < 0) {
+                langs.push(lang);
+            }
+        });
+    });
+
+    let rows = [];
+
+    // First row is header
+    rows.push([ 'File path', 'YAML path' ].concat(langs));
+
+    Object.keys(messages).forEach(msg => {
+        let message = messages[msg];
+        let translations = langs.map(lang => message.translations[lang] || '');
+        let row = [ message.filePath, message.yamlPath ].concat(translations);
+
+        rows.push(row);
+    });
+
+    stringify(rows, { delimiter: '\t' }, (err, csv) => {
+        console.log(csv);
+    });
+});
