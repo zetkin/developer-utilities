8569a2a43dd7a33c019969bdd584844c90b3bd92
Olle Jonsson <olle.jonsson@gmail.com>
2018-11-10 15:42:05 +0100

    Rename a script to a clearer name

--- l10n/yml2tsv/add-and-update-project-terms.sh
+++ l10n/yml2tsv/add-and-update-project-terms.sh
@@ -7,7 +7,7 @@ source ../.env
 
 export APP_LOCALE_PATH=${APP_LOCALE_PATH:-~/opensource/www.zetk.in/locale}
 
-TERMS=$(npm run --silent updateTerms)
+TERMS=$(npm run --silent showEnglishTerms)
 
 echo "Add Terms to project ${POEDITOR_PROJECT_ID}"
 
@@ -25,3 +25,11 @@ curl -X POST https://api.poeditor.com/v2/terms/update \
      -d id="${POEDITOR_PROJECT_ID}" \
      -d data="${TERMS}" \
      -d fuzzy_trigger="1" | jq .
+
+# echo "Delete terms from project ${POEDITOR_PROJECT_ID}"
+#
+# curl -X POST https://api.poeditor.com/v2/terms/delete \
+#      -H "Content-Type: application/x-www-form-urlencoded; charset=utf-8" \
+#      -d api_token="${POEDITOR_API_KEY}" \
+#      -d id="${POEDITOR_PROJECT_ID}" \
+#      -d data="${TERMS}" | jq .
